ffe301b52903f775dfdb358d459896cdaecbb69d
gladysprds <gladyscaprds@gmail.com>
2022-02-15 16:18:54 +0700

    third commit

--- halo.php
+++ halo.php
@@ -2,6 +2,7 @@
  halo ini gladys
    saya berkuliah di it del
    Nim saya 11319011
-   sekarang saya semester 6 
+   sekarang saya semester 6
+   saya mengikuti studi independen di altera dan itu sangat menantang say untuk lebih maju 
 
         ?>
